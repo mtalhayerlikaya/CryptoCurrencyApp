03ed3745e2c871fd3f7e6c9aa2ca81a8138b602f
mtalhayerlikaya <mtalhayerlikaya@gmail.com>
2023-06-16 11:27:00 +0300

    glide implemented and recyclerview improved

--- app/src/main/java/com/example/cryptocurrencyapp/view/home/CryptoAdapter.kt
+++ app/src/main/java/com/example/cryptocurrencyapp/view/home/CryptoAdapter.kt
@@ -4,25 +4,31 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.cryptocurrencyapp.R
 import com.example.cryptocurrencyapp.data.model.CryptoModel
 import com.example.cryptocurrencyapp.databinding.HomeRecyclerviewItemBinding
 
-class CryptoAdapter(val contect: Context, private val cryptoList: List<CryptoModel>) :
+class CryptoAdapter(val context: Context, private val cryptoList: List<CryptoModel>) :
     RecyclerView.Adapter<CryptoAdapter.CryptoViewHolder>() {
     inner class CryptoViewHolder(var binding: HomeRecyclerviewItemBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(cryptoModel: CryptoModel) {
             binding.homeRecyclerviewCryptoName.text = cryptoModel.name
-            binding.homeRecyclerviewCryptoSymbol.text = cryptoModel.symbol
-            binding.homeRecyclerviewCrypto24hrPercentage.text = cryptoModel.price_change_percentage_24h
+            binding.homeRecyclerviewCryptoSymbol.text = cryptoModel.symbol.uppercase()
+            binding.homeRecyclerviewCrypto24hrPercentage.text = context.resources
+                .getString(R.string.crypto_price_with_percentage,cryptoModel.price_change_percentage_24h,"%")
             binding.homeRecyclerviewCryptoCurrentPrice.text = cryptoModel.current_price
             if(!cryptoModel.price_change_percentage_24h.startsWith("-")){
                 binding.homeRecyclerviewCrypto24hrPercentage
-                    .setTextColor(contect.resources.getColor(R.color.edittext_gain_percentage_color,contect.theme))
+                    .setTextColor(context.resources.getColor(R.color.edittext_gain_percentage_color,context.theme))
             }else{
                 binding.homeRecyclerviewCrypto24hrPercentage
-                    .setTextColor(contect.resources.getColor(R.color.edittext_lost_percentage_color,contect.theme))
+                    .setTextColor(context.resources.getColor(R.color.edittext_lost_percentage_color,context.theme))
             }
+            Glide.with(context)
+                .load(cryptoModel.image)
+                //.placeholder(R.drawable.piwo_48)
+                .into(binding.homeRecyclerviewCryptoImage);
         }
     }
 
