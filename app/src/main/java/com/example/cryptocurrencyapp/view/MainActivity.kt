681cf909f6e6d44b6b43bf75eb928be9d86aa124
mtalhayerlikaya <mtalhayerlikaya@gmail.com>
2023-06-18 15:07:57 +0300

    nav host container visibility

--- app/src/main/java/com/example/cryptocurrencyapp/view/MainActivity.kt
+++ app/src/main/java/com/example/cryptocurrencyapp/view/MainActivity.kt
@@ -1,21 +1,36 @@
 package com.example.cryptocurrencyapp.view
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.example.cryptocurrencyapp.R
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.example.cryptocurrencyapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
-        val navController = findNavController(R.id.fragmentContainerView)
-        bottomNavigationView.setupWithNavController(navController)
-      
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        NavigationUI.setupWithNavController(binding.bottomNavigationView, navHostFragment.navController)
+        setnavHostDestinateionChangedListener(navHostFragment)
+    }
+
+    private fun setnavHostDestinateionChangedListener(navHostFragment: NavHostFragment) {
+        navHostFragment.navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id == R.id.detailFragment) {
+                binding.bottomNavigationView.visibility = View.GONE
+            } else {
+                binding.bottomNavigationView.visibility = View.VISIBLE
+            }
+        }
     }
 }
